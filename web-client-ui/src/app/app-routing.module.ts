277961402c012a53b5f9d16e840ad3ed30f18477
dansotirakis <dansotirakis18@gmail.com>
2019-08-22 17:42:47 -0300

    Update app-routing.module.ts

--- web-client-ui/src/app/app-routing.module.ts
+++ web-client-ui/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { CreateClienteComponent } from './cliente-add/cliente-add.component';
-import { ClienteDetailsComponent } from './cliente-detail/cliente-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'cliente', pathMatch: 'full' },
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
   { path: 'clientes', component: ClienteListComponent },
   { path: 'add', component: CreateClienteComponent },
-  { path: 'details/:id', component: ClienteDetailsComponent },
 ];
 
 @NgModule({
