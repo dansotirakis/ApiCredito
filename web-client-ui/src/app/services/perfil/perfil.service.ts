7732ba0bf2e1349dab8a0c4810ba062c76d7269e
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-08-24 23:01:58 -0300

    Update layout order css

--- web-client-ui/src/app/services/perfil/perfil.service.ts
+++ web-client-ui/src/app/services/perfil/perfil.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
+  
+  private baseUrl = 'http://localhost:8180/perfil';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+    updatePerfil(id: number, value: any): Observable < Object > {
+      return this.http.put(`${this.baseUrl}/${id}`, value);
+    }
+
+    deletePerfil(id: number): Observable < any > {
+      return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+    }
+
+    getPerfilsList(): Observable < any > {
+      return this.http.get(`${this.baseUrl}/list`);
+    }
+
+    createPerfil(Perfil: Object): Observable < Object > {
+      return this.http.post(`${this.baseUrl}/add`, Perfil);
+    }
+
+    getPerfil(id: number): Observable < any > {
+      return this.http.get(`${this.baseUrl}/${id}`);
+    }
+  
 }
